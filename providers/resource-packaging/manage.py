a23b909525cadd6f7fd4ac7939c0d2d96720e764
Sylvain Pineau <sylvain.pineau@canonical.com>
2016-10-07 10:07:47 +0200

    Import plainbox-provider-resource-generic_0.17.orig.tar.gz

--- providers/resource-packaging/manage.py
+++ providers/resource-packaging/manage.py
@@ -44,7 +44,8 @@ class SourceDistributionCommandExt(SourceDistributionCommand):
 
 setup(
     name='2013.com.canonical.certification:plainbox-resources',
-    version="0.3",
+    version="0.17",
     description=N_("PlainBox resources provider"),
-    gettext_domain='2013.com.canonical.certification.plainbox-resources',
+    gettext_domain='plainbox-provider-resource',
+    deprecated=False, strict=False,
 )
